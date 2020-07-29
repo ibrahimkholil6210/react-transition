e74b4805a5a3b9164f7b717d5078cb936052d6c6
Ibrahim <ibrahimkholil550@gmail.com>
2020-07-29 21:27:05 +0600

    Added CSSTransition property to add custom animation classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import CSSTransition from 'react-transition-group/CSSTransition';
 import Transition from 'react-transition-group/Transition';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -35,18 +36,19 @@ class App extends Component {
         <div>
           <button className="Button" onClick={this.handleClick}>Toggle</button>
         </div>
-        <Transition in={this.state.showBlock} timeout={300} mountOnEnter unmountOnExit>
-          {state => (
-            <div
-              style={{
-                backgroundColor: "red",
-                width: 100, height: 100,
-                margin: "auto",
-                opacity: state === 'exiting' ? 0 : 1,
-                transition: "opacity 1s ease-out"
-              }}>
-            </div>)}
-        </Transition>
+        <CSSTransition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          classNames={{
+            enterActive: 'BlockOpen',
+            exitActive: 'BlockClose'
+          }}
+        >
+          <div className="Block">
+          </div>
+        </CSSTransition>
         <Transition in={this.state.showModal} timeout={500} mountOnEnter unmountOnExit>
           {state => (
             <Modal show={state} closed={this.closeModal} />
