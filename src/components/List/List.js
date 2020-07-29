d2c739a5e8dd7e705445acd4864d9382aee36df9
Ibrahim <ibrahimkholil550@gmail.com>
2020-07-29 21:38:23 +0600

    Added TransitionGroup

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import TransitionGroup from 'react-transition-group/TransitionGroup';
+import CSSTransition from 'react-transition-group/CSSTransition';
 import './List.css';
 
 class List extends Component {
@@ -23,21 +24,27 @@ class List extends Component {
         });
     }
 
-    render () {
-        const listItems = this.state.items.map( (item, index) => (
-            <li 
+    render() {
+        const listItems = this.state.items.map((item, index) => (
+            <CSSTransition
                 key={index}
-                className="ListItem" 
-                onClick={() => this.removeItemHandler(index)}>{item}</li>
-        ) );
+                timeout={300}
+                classNames="fade"
+            >
+                <li
+                    className="ListItem"
+                    onClick={() => this.removeItemHandler(index)}>{item}</li>
+            </CSSTransition>
+
+        ));
 
         return (
             <div>
                 <button className="Button" onClick={this.addItemHandler}>Add Item</button>
                 <p>Click Item to Remove.</p>
-                <ul className="List">
+                <TransitionGroup component="ul" className="List">
                     {listItems}
-                </ul>
+                </TransitionGroup>
             </div>
         );
     }
